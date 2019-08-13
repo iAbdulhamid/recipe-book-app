8facbbc462e7b57d3991e99be49d1a919ab371e6
iAbdulhamid <i_abdulhamid@outlook.com>
2019-08-13 16:06:47 +0200

    Passing Ingredients to the Shopping List

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, ViewChild, ElementRef, Output } from '@angular/core';
+import { Ingredient } from 'src/app/Shared Classes and Interfaces/ingredient';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  onItemAdded() {
+    const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
+    this.ingredientAdded.emit(newIngredient);
+  }
+
+
 
 }
